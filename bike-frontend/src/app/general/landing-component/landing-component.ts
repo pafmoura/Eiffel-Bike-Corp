6d53bf2fa6a29a5953828404ae1277fc9f2fe2c7
Pedro Moura <100920362+pafmoura@users.noreply.github.com>
2025-12-23 11:25:25 +0000

    Favicon and Landing Page links Fixed

--- bike-frontend/src/app/general/landing-component/landing-component.ts
+++ bike-frontend/src/app/general/landing-component/landing-component.ts
@@ -1,9 +1,12 @@
 // landing-component.ts
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-component',
   templateUrl: './landing-component.html',
   styleUrl: './landing-component.scss',
+  imports: [RouterModule],
+  standalone: true,
 })
 export class LandingComponent {}
