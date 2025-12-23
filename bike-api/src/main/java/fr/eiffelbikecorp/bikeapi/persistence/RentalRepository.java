16c047e4b1014b4be9a9b69b3f6fd0abc2ba64c7
Pedro Moura <100920362+pafmoura@users.noreply.github.com>
2025-12-23 10:06:41 +0000

    MyRentals Repository Changes

--- bike-api/src/main/java/fr/eiffelbikecorp/bikeapi/persistence/RentalRepository.java
+++ bike-api/src/main/java/fr/eiffelbikecorp/bikeapi/persistence/RentalRepository.java
@@ -4,9 +4,13 @@ import fr.eiffelbikecorp.bikeapi.domain.entity.Rental;
 import fr.eiffelbikecorp.bikeapi.domain.enums.RentalStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
     boolean existsByBike_IdAndStatus(Long bikeId, RentalStatus status);
 
     boolean existsByBike_Id(Long bikeId);
+    List<Rental> findByCustomer_IdAndStatusIn(UUID customerId, List<RentalStatus> statuses);
 }
