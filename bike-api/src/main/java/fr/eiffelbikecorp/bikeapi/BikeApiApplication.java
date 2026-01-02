eb1ec7cde6448caf1c9e08844ad81ff7b883ab68
Marcos Duchinski <92726479+marcosduchinski@users.noreply.github.com>
2026-01-02 23:06:24 +0000

    Enable asynchronous for sending async emails

--- bike-api/src/main/java/fr/eiffelbikecorp/bikeapi/BikeApiApplication.java
+++ bike-api/src/main/java/fr/eiffelbikecorp/bikeapi/BikeApiApplication.java
@@ -5,9 +5,11 @@ import fr.eiffelbikecorp.bikeapi.configuration.StripeProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ExchangeRateApiProperties.class, StripeProperties.class})
+@EnableAsync
 public class BikeApiApplication {
 
     public static void main(String[] args) {
