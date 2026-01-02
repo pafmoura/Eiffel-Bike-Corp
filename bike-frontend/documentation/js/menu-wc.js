dea80ce5d727591d636d132253a0cb37468a6228
Pedro Moura <100920362+pafmoura@users.noreply.github.com>
2026-01-02 11:38:54 +0000

    COMPODOC: Add generated documentation for bike-frontend
    Adds HTML documentation files for components, interfaces, injectables, styles, images, fonts, and other assets under bike-frontend/documentation.

---
+++ bike-frontend/documentation/js/menu-wc.js
@@ -0,0 +1,171 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">bike-frontend documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/Alert.html" data-type="entity-link" >Alert</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/App.html" data-type="entity-link" >App</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Dashboard.html" data-type="entity-link" >Dashboard</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LandingComponent.html" data-type="entity-link" >LandingComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Loginpage.html" data-type="entity-link" >Loginpage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Mainlayout.html" data-type="entity-link" >Mainlayout</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MarketplaceComponent.html" data-type="entity-link" >MarketplaceComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Myrentals.html" data-type="entity-link" >Myrentals</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/OfferBikeComponent.html" data-type="entity-link" >OfferBikeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Register.html" data-type="entity-link" >Register</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/Bikeapi.html" data-type="entity-link" >Bikeapi</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/BikeMarketplace.html" data-type="entity-link" >BikeMarketplace</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/FxRateService.html" data-type="entity-link" >FxRateService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UserService.html" data-type="entity-link" >UserService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/ActiveRentalDto.html" data-type="entity-link" >ActiveRentalDto</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/AlertState.html" data-type="entity-link" >AlertState</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/AlertState-1.html" data-type="entity-link" >AlertState</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/RentalResponse.html" data-type="entity-link" >RentalResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/WaitlistDto.html" data-type="entity-link" >WaitlistDto</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
